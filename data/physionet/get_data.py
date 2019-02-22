618018b97529aea001c5c8e26917b8b8cec989d5
Lisa Ong <issohl@nus.edu.sg>
2019-02-22 15:17:31 +0800

    more robust

--- data/physionet/get_data.py
+++ data/physionet/get_data.py
@@ -1,9 +1,13 @@
+import os 
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
 import matplotlib.pyplot as plt
 
 # https://github.com/MIT-LCP/wfdb-python
 # pip install wfdb
 import wfdb
 
-record = wfdb.rdrecord('./data/physionet/Subject10_AccTempEDA')
+record = wfdb.rdrecord(os.path.join(DIR_PATH, 'Subject10_AccTempEDA'))
+print(record.__dict__)
+
 wfdb.plot_wfdb(record=record, title='Subject10_AccTempEDA')
-display(record.__dict__)
